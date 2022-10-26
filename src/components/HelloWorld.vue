d16f78ff6f1e228523316ed2e3fa4023b9ae9301
JJyun00 <113875839+JJyun00@users.noreply.github.com>
2022-10-26 15:44:22 +0900

    기간 선택 후 날짜 검색(완성X)
    출력된 데이터에서 검색한 해당 날짜 데이터만 보이게 하려했는데, for문을 계속 타는 문제? showAll문제?

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,13 +11,30 @@
     <div v-if='view === true'>
       <h2>전체 결과 수 : {{ totalCount }}</h2>
       <p>--------------------------------------------------------------------</p>
-        <!-- 날짜검색 -->
-        <input type="date" @input="searchdate = $event.target.value"/>
+      <!-- 날짜검색 -->
+      <div>
+        <input type="date" :min="startnum" :max="endnum" @input="searchdate = $event.target.value"/>
         <button type="button" @click="search">검색</button>
-        <!-- -------- -->
+        <p v-show="!showEmpty">{{ empty }}</p>
+        
+        <div v-show="!showAll">
+          <p class="searchC">2 기준일 : {{ state_Dt }}</p>
+          <p class="searchC">2 확진자 수 : {{ decide_Cnt }}</p>
+          <p class="searchC">2 사망자 수 : {{ death_Cnt }}</p>
+          <p>=======================================</p>
+        </div>
+      </div> 
+      
+      <!-- filter -->
+      <!-- <div>
+        <input type="date" @input="searchdate = $event.target.value">
+        <button type="button" @click="search">검색</button>
+        <p>{{filterText | searchFilter}}</p>
+      </div> -->
 
-      <div v-for="item in array" :key="item.SEQ">
-        <p :class="test? 'aclass':'class'">기준일 : {{ item.stateDt }}</p>
+      <div v-for="item in array" :key="item.stateDt" v-show="showAll">
+        <!-- <p v-show="showEmpty">{{ empty }}</p> -->
+        <p>기준일 : {{ item.stateDt }}</p>
         <p>확진자 수 : {{ item.decideCnt }}</p>
         <p>사망자 수 : {{ item.deathCnt }}</p>
         <p>=======================================</p>
@@ -34,12 +51,28 @@ import axios from 'axios'
 export default {
 name: 'HelloWorld',
 
+filters:{
+  searchFilter: function(){
+
+  }
+},
+
 data() {
   return {
     view: true,
     totalCount: '',
     array:'',
-    test:true,
+    searchArray:'',
+    showAll:true,
+    empty:'is Empty!!',
+    showEmpty:true,
+    // 날짜검색
+    state_Dt:'',
+    decide_Cnt:'',
+    death_Cnt:'',
+    startnum:'',
+    endnum:'',
+    searchC:''
   }
 },
 
@@ -55,7 +88,6 @@ methods: {
     axios.get(url + key + param + start + end)
     .then((result) => {
       console.log(result.data)
-
       if(result.data.response.body.items.length < 1) {
         lv_Vm.fn_isEmpty()
       }
@@ -72,32 +104,28 @@ methods: {
 
   search() {
     const a = this
+    a.searchArray = [...a.array]
     for(let i=0; i<a.array.length; i++){
-      if(a.searchdate.split('-').join('') === a.array[i].stateDt.toString()){
-        a.test=false
-        console.log(a.searchdate.split('-').join(''))
-        console.log(a.array[i].stateDt.toString())
+      if(a.searchdate.split('-').join('') === a.searchArray[i].stateDt.toString()){
+        a.showAll = true
+        a.showEmpty = false
+        a.state_Dt = a.searchArray[i].stateDt
+        a.decide_Cnt = a.searchArray[i].decideCnt
+        a.death_Cnt = a.searchArray[i].deathCnt
       }
       else {
-        a.test=true 
-        console.log('failfail')
+        a.showAll = false
+        a.showEmpty = true
       }
     }
-    
-  },
 
-  fn_isEmpty() {
-    console.log('isEmpty!!')
+
   }
 }
 }
 </script>
 
 <style scoped>
-.aclass {
-  color:red;
+.searchC { 
+  color: red;
 }
-.class{
-  color:blue;
-}
-</style>

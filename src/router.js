2bbf819a988b433897349cbdd98acb630ff2ee55
JJyun00 <113875839+JJyun00@users.noreply.github.com>
2022-11-02 17:04:23 +0900

    Add files via upload

---
+++ src/router.js
@@ -0,0 +1,13 @@
+
+import DashBoard from './components/DashBoard.vue';
+import UserLog from './components/UserLog.vue';
+import LogView from './components/LogView.vue';
+import InFo from './components/InFo.vue';
+
+export default[
+  {path:'/DashBoard', name:'DashBoard', component:DashBoard},
+  {path:'/UserLog', name:'UserLog', component:UserLog},
+  {path:'/LogView', name:'LogView', component:LogView},
+  {path:'/InFo', name:'InFo', component:InFo}
+]
+

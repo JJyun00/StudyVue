b80d57f6daf10bd809a6127e9433fa0be911ae6e
JJyun00 <113875839+JJyun00@users.noreply.github.com>
2022-11-02 17:06:31 +0900

    컴포넌트 간 데이터 전달

--- src/components/InFo.vue
+++ src/components/InFo.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
         <div>
-            <p>ID : {{$route.params.id}}</p>
-            <p>이름 : {{$route.params.name}}</p>
-            <p>나이 : {{$route.params.age}}</p>
-            <p>전화번호 : {{$route.params.phone}}</p>
-            <p>이메일 : {{ $route.params.email }}</p>
-            <p>주소 : {{ $route.params.adress }}</p>
+            <p>ID : {{id}}</p>
+            <p>이름 : {{name}}</p>
+            <p>나이 : {{age}}</p>
+            <p>전화번호 : {{phone}}</p>
+            <p>이메일 : {{email }}</p>
+            <p>주소 : {{adress }}</p>
         </div>
+        <hr/>
         <div><button @click="simple"> 간략히 </button></div>
     </div>
 </template>
@@ -15,17 +16,32 @@
 <script>
     export default {
         name:'InFo',
+        computed:{
+            id(){ return this.$route.params.id },
+            name(){ return this.$route.params.name },
+            age(){ return this.$route.params.age },
+            phone(){ return this.$route.params.phone },
+            email(){ return this.$route.params.email },
+            adress(){ return this.$route.params.adress },
+        },
         data() {
             return{
             }
         },
-        props: {
-            name:{type:String, default:''},
-            age :{type:Number, default:0}
-        },
         methods: {
             simple() {
-                // this.$router.push({name:'LogView'}).catch(()=>{})
+                this.$router.push({
+                    name:'LogView',
+                    params: {
+                    id: this.id, 
+                    name: this.name,
+                    age: this.age,
+                    phone: this.phone,
+                    email: this.email,
+                    adress: this.adress,
+                    }
+                })
+                .catch(()=>{})
                 
             }
         }

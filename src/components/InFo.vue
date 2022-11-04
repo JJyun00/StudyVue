58bb02e07c52d249b6ae391d91866c2f45eae783
JJyun00 <113875839+JJyun00@users.noreply.github.com>
2022-11-04 13:32:29 +0900

    정보 수정, 라이프사이클훅

--- src/components/InFo.vue
+++ src/components/InFo.vue
@@ -1,15 +1,18 @@
+
+<!-- 정보 수정 -->
+
 <template>
     <div>
         <div>
-            <p>ID : {{id}}</p>
-            <p>이름 : {{name}}</p>
-            <p>나이 : {{age}}</p>
-            <p>전화번호 : {{phone}}</p>
-            <p>이메일 : {{email }}</p>
-            <p>주소 : {{adress }}</p>
+            <p>ID : {{id}} </p>
+            <p> 이름 : <input type="text" @input="name=$event.target.value"> </p>
+            <p> 나이 : <input type="text" @input="age=$event.target.value"> </p>
+            <p> 전화번호 : <input type="text" @input="phone=$event.target.value"> </p>
+            <p> 이메일 : <input type="text" @input="email=$event.target.value"> </p>
+            <p> 주소 : <input type="text" @input="adress=$event.target.value"> </p>
         </div>
         <hr/>
-        <div><button @click="simple"> 간략히 </button></div>
+        <div><button @click="change"> 확인 </button></div>
     </div>
 </template>
 
@@ -18,18 +21,9 @@
         name:'InFo',
         computed:{
             id(){ return this.$route.params.id },
-            name(){ return this.$route.params.name },
-            age(){ return this.$route.params.age },
-            phone(){ return this.$route.params.phone },
-            email(){ return this.$route.params.email },
-            adress(){ return this.$route.params.adress },
-        },
-        data() {
-            return{
-            }
         },
         methods: {
-            simple() {
+            change() {
                 this.$router.push({
                     name:'LogView',
                     params: {
@@ -42,8 +36,13 @@
                     }
                 })
                 .catch(()=>{})
-                
             }
+        },
+        created() {
+            alert("정보를 수정하시겠습니까?")
+        },
+        beforeDestroy() {
+            alert("정보가 수정되었습니다.")
         }
     }
     

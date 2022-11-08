1b0b21d0a8dde51632c74cde93ead8af2778cca2
JJyun00 <113875839+JJyun00@users.noreply.github.com>
2022-11-08 14:23:18 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router/index.js'
+import axios from "axios"
+import VueRouter from 'vue-router'
+import Routes from './router.js'
+
+Vue.use(VueRouter)
+Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
+
+const router = new VueRouter({
+  routes: Routes,
+  mode : 'history' //history 모드
+});
 
 new Vue({
-  router,
   render: h => h(App),
+  router: router
 }).$mount('#app')

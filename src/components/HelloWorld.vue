78aad7c1c0949def243ecf44f8037e59e81f61ac
JJyun00 <113875839+JJyun00@users.noreply.github.com>
2022-11-08 14:18:24 +0900

    박승환대리님 코드

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,131 +1,198 @@
 <template>
-  <div>
-    <!-- 달력날짜선택 -->
+  <div class="hello">
+    <h1>Vue Study</h1>
+
+    <div><input type="text" v-model="textModel" name="text1" id="text1" placeholder="입력해주세요"></div>
+    {{showText}}
+    <br><br>
+    <span>리스트 체크: {{inputTag2}}</span><br>
     <div>
-      Start Date   <input type="date" max="2022-10-26" @input="startnum = $event.target.value"/><br/>
-      End Date   <input type="date" min="" max="2022-10-26" @input="endnum = $event.target.value"/>
+      <input type="text" v-model="pushValue" name="text1" id="text1">
+      <button @click="fn_push(pushValue)">PUSH!</button>
     </div>
-    <!------------------>
+    
+    <div v-for="item in data1" :key="item.key">
+      <p>key: {{item.key}} value: {{item.value}}</p>
+    </div>
 
-    <br/><button type="button" @click="searchall">보기</button>
-    <div v-if='view === true'>
-      <h2>전체 결과 수 : {{ totalCount }}</h2>
-      <p>--------------------------------------------------------------------</p>
-      <!-- 날짜검색 -->
+    <div><button @click="fn_test">POP!</button></div><br>
+    
+    
+    
+    <input type="date" v-model="startDate" name="date1" id="date1">
+    ~ <input type="date" v-model="endDate" name="date2" id="date2">
+    <button @click="fn_post">통신!</button><br><br>
+    <div>
+      <input type="text" v-model="inputTag" name="text2" id="text2" />
+      <input type="date" v-model="inputDate" name="date3" id="date3">
+    </div>
+    <div>
+      <button @click="fn_filter(inputTag)">필터!</button>
+      <button @click="fn_filter(inputDate)">달력검색!</button>
+    </div>
+
+    <br/>
+    <br/>
+    <span><p>{{stateMsg}}</p></span>
+    <div v-for="item in clonData" :key="item.createDt">
       <div>
-        <input type="date" :min="startnum" :max="endnum" @input="searchdate = $event.target.value"/>
-        <button type="button" @click="search">검색</button>
-        <p v-show="!showEmpty">{{ empty }}</p>
-        
-        <div v-show="!showAll">
-          <p class="searchC">2 기준일 : {{ state_Dt }}</p>
-          <p class="searchC">2 확진자 수 : {{ decide_Cnt }}</p>
-          <p class="searchC">2 사망자 수 : {{ death_Cnt }}</p>
-          <p>=======================================</p>
-        </div>
-      </div> 
-      
-      <!-- filter -->
-      <!-- <div>
-        <input type="date" @input="searchdate = $event.target.value">
-        <button type="button" @click="search">검색</button>
-        <p>{{filterText | searchFilter}}</p>
-      </div> -->
-
-      <div v-for="item in array" :key="item.stateDt" v-show="showAll">
-        <!-- <p v-show="showEmpty">{{ empty }}</p> -->
-        <p>기준일 : {{ item.stateDt }}</p>
-        <p>확진자 수 : {{ item.decideCnt }}</p>
-        <p>사망자 수 : {{ item.deathCnt }}</p>
-        <p>=======================================</p>
+        <hr/>
+        <p>날짜: {{item.createDt}}</p>
+        <p>확진자수: {{item.decideCnt}}</p>
+        <p>사망수: {{item.deathCnt}}</p>
       </div>
     </div>
-
-    
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-
+import axios from "axios";
 export default {
-name: 'HelloWorld',
-
-filters:{
-  searchFilter: function(){
-
-  }
-},
-
-data() {
-  return {
-    view: true,
-    totalCount: '',
-    array:'',
-    searchArray:'',
-    showAll:true,
-    empty:'is Empty!!',
-    showEmpty:true,
-    // 날짜검색
-    state_Dt:'',
-    decide_Cnt:'',
-    death_Cnt:'',
-    startnum:'',
-    endnum:'',
-    searchC:''
-  }
-},
-
-methods: {
-  searchall() {
-    let url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?'
-    let key = 'serviceKey=D%2FO%2Bxhp4RTql%2Fgcl1JK12cV%2BaW0CyCHsEAmjP%2F%2BrzTOVlp9OW1v55KtZYWihwA%2BkvoNw5Yl4Az46V8cu7t99fg%3D%3D'
-    let param = '&pageNo=1&numOfRows=10'
-    let start = '&startCreateDt='+ this.startnum
-    let end = '&endCreateDt=' + this.endnum
-
-    const lv_Vm = this
-    axios.get(url + key + param + start + end)
-    .then((result) => {
-      console.log(result.data)
-      if(result.data.response.body.items.length < 1) {
-        lv_Vm.fn_isEmpty()
-      }
-      lv_Vm.totalCount = result.data.response.body.totalCount
-      lv_Vm.array= result.data.response.body.items.item
-      lv_Vm.view = true
-      console.log(result)
-    })
-
-    .catch((error) => {
-      console.log(error)
-    })
+  name: 'HelloWorld',
+  props: {
+    msg: String
   },
-
-  search() {
-    const a = this
-    a.searchArray = [...a.array]
-    for(let i=0; i<a.array.length; i++){
-      if(a.searchdate.split('-').join('') === a.searchArray[i].stateDt.toString()){
-        a.showAll = true
-        a.showEmpty = false
-        a.state_Dt = a.searchArray[i].stateDt
-        a.decide_Cnt = a.searchArray[i].decideCnt
-        a.death_Cnt = a.searchArray[i].deathCnt
-      }
-      else {
-        a.showAll = false
-        a.showEmpty = true
-      }
+  watch: {
+    textModel(txt) {
+      this.showText = txt
+    },
+    inputTag(txt) {
+      this.test = txt
+    },
+    orgData(data) {
+      this.clonData = data
+      this.fn_resetData()
+      console.log(this.clonData)
     }
+  },
+  computed: {
+    inputTag2() {
+      return this.data1.length>0 ? 'O' :'X'
+    },
+    stateMsg() {
+      return this.clonData.length < 1 ? '데이터가 존재하지 않습니다.' : ''
+    }
+  },
+  // 데이터 셋팅
+  data() {
+    return {
+      textModel: '',
+      showText: '',
+      pushValue: '',
+      inputTag: '',
+      inputDate: '2022-10-21',
+      startDate: '',
+      endDate: '',
+      data1: [
+          {key:1, value:'a'},
+          {key:2, value:'b'},
+          {key:3, value:'c'},
+          {key:4, value:'d'},
+      ],
+      orgData: [],
+      clonData: [],
+      isData: false,
+      testBoolean: true
+    }
+  },
+  created() {
+    this.fn_init()
+  },
+  methods: {
+    fn_init() {
+      let today = new Date()
+      let month = today.getMonth()+1 < 10 ? '0'+today.getMonth()+1 : today.getMonth()+1
+      this.endDate   = today.getFullYear() + '-' + month + '-' + today.getDate()
+      this.startDate = today.getFullYear() + '-' + month + '-' + (today.getDate() - 1)
+    },
+    fn_test() {
+      this.data1.pop()
+    },
+    fn_post() {
+      const lv_Vm = this
+      if(this.startDate === this.endDate) {
+        alert('하루 이상 조회하세요')
+        return
+      }
+      console.log('startDate', this.startDate.split('-').join(''))
+      console.log('endDate', this.endDate.split('-').join(''))
+      let url = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=D%2FO%2Bxhp4RTql%2Fgcl1JK12cV%2BaW0CyCHsEAmjP%2F%2BrzTOVlp9OW1v55KtZYWihwA%2BkvoNw5Yl4Az46V8cu7t99fg%3D%3D"
+      let params = {
+        pageNo: 1,
+        numOfRows: 10,
+        startCreateDt: this.startDate,
+        endCreateDt: this.endDate
+      }
+      let urlParam = '&pageNo='+params.pageNo+'&numOfRows='+params.numOfRows+'&startCreateDt='+params.startCreateDt+'&endCreateDt='+params.endCreateDt
 
+      axios.get(url+urlParam).then((result)=> {
+        console.log('result', result)
+        lv_Vm.orgData = result.data.response.body.items.item
+        if(lv_Vm.orgData > 0) {
+          lv_Vm.isData = true
+        } else {
+          lv_Vm.orgData = [lv_Vm.orgData]
+          lv_Vm.isData = false
+        }
+        console.log('orgData', lv_Vm.orgData)
+      })
+    },
+    fn_resetData(){
+      let item = {}
+      let Data = this.clonData
+      if(this.orgData[0].length > 0) Data = this.orgData[0]
+      
+      for(let i=0; i<Data.length; i++) {
+        item = Data[i]
 
+        item.createDt = Data[i].createDt.substr(0,10)
+        item.decideCnt = Data[i].decideCnt.toLocaleString('ko-KR')
+        item.deathCnt = Data[i].deathCnt.toLocaleString('ko-KR')
+      }
+      this.orgData = Data
+      this.clonData = Data
+    },
+    fn_filter(keyword) {
+
+      this.clonData = this.orgData.filter((item)=>
+        item.createDt.indexOf(keyword) > -1
+      )
+
+      if(this.clonData.length < 1) {
+        alert('데이터가 존재하지 않습니다.')
+      }
+    },
+    fn_push(value) {
+      if(value === '') {
+        alert('입력값이 없습니다.')
+        return
+      }
+      let idx = this.data1.length + 1
+      this.data1.push({key: idx, value: value})
+      this.pushValue = ''
+    },
+    fn_submit() {
+      let msg = '데이터 보냅니다.'
+      this.$emit('msgSubmit',msg);
+    }
   }
-}
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.searchC { 
-  color: red;
+h3 {
+  margin: 40px 0 0;
 }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

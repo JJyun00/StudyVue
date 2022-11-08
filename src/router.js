6a3535ec945554680bb6780db5f3a49887748807
JJyun00 <113875839+JJyun00@users.noreply.github.com>
2022-11-08 14:26:56 +0900

    Update router.js

--- src/router.js
+++ src/router.js
@@ -3,11 +3,17 @@ import DashBoard from './components/DashBoard.vue';
 import UserLog from './components/UserLog.vue';
 import LogView from './components/LogView.vue';
 import InFo from './components/InFo.vue';
+import TesT from './components/TesT.vue';
+import SimPle from './components/SimPle.vue';
+import MoRe from './components/MoRe.vue';
 
 export default[
   {path:'/DashBoard', name:'DashBoard', component:DashBoard},
   {path:'/UserLog', name:'UserLog', component:UserLog},
   {path:'/LogView', name:'LogView', component:LogView},
-  {path:'/InFo', name:'InFo', component:InFo}
+  {path:'/InFo', name:'InFo', component:InFo},
+  {path:'/TesT', name:'TesT', component:TesT},
+  {path:'/SimPle', name:'SimPle', component:SimPle},
+  {path:'/MoRe', name:'MoRe', component:MoRe}
 ]
 
